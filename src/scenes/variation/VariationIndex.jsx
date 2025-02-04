19ca9fb93319a907ea4305dada3a58608cecbfc2
Uditha20 <udithaindunil5@gmail.com>
2025-02-04 21:23:02 +0530

    changes

--- src/scenes/variation/VariationIndex.jsx
+++ src/scenes/variation/VariationIndex.jsx
@@ -15,155 +15,346 @@ import {
 } from "@mui/material";
 import Header from "components/Header";
 import VariationFrom from "./Variation";
-import { useGetVariationQuery,useGetProductsQuery } from "../../state/api";
+import { useGetVariationQuery, useGetProductsQuery } from "../../state/api";
 function VariationIndex() {
   const [productId, setProductId] = useState("");
-    const [isAddProductOpen, setIsAddProductOpen] = useState(false);
-    const { data, isLoading } = useGetVariationQuery();
-    const { data: products, isLoading: isProductsLoading } =
+  const [isAddProductOpen, setIsAddProductOpen] = useState(false);
+  const { data, isLoading } = useGetVariationQuery();
+  const { data: products, isLoading: isProductsLoading } =
     useGetProductsQuery();
-    const filteredVariations = Array.isArray(data)
+  const filteredVariations = Array.isArray(data)
     ? data.filter((item) => item.productId?._id === productId)
     : [];
-    const baseUrl = process.env.REACT_APP_BASE_URL;
-    const handleOpenAddProduct = () => setIsAddProductOpen(true);
-    const handleCloseAddProduct = () => setIsAddProductOpen(false);
-  
-    // State to control image modal
-    const [openImageModal, setOpenImageModal] = useState(false);
-    const [selectedImages, setSelectedImages] = useState([]);
-  
-    const handleOpenImageModal = (mainImage, additionalImages) => {
-      setSelectedImages([mainImage, ...additionalImages]);
-      setOpenImageModal(true);
-    };
-  // console.log(selectedImages)
-    const handleCloseImageModal = () => {
-      setOpenImageModal(false);
-      setSelectedImages([]);
-    };
-  
-    if (isLoading) return <Typography>Loading...</Typography>;
-  console.log(data)
+  const baseUrl = process.env.REACT_APP_BASE_URL;
+  const handleOpenAddProduct = () => setIsAddProductOpen(true);
+  const [isEditProductOpen, setIsEditProductOpen] = useState(false);
+  const [selectedVariation, setSelectedVariation] = useState(null);
+  const handleCloseAddProduct = () => setIsAddProductOpen(false);
+  const [imagePreview, setImagePreview] = useState(null);
 
-    return (
-      <Box>
-        <Header title="Variation" subtitle="See your list of products." />
-        <Button variant="contained" onClick={handleOpenAddProduct}>
-          Add Variation
-        </Button>
-        <VariationFrom
-          open={isAddProductOpen}
-          handleClose={handleCloseAddProduct}
-        />
-        <Box sx={{ p: 2 }}>
-          <Typography variant="h5">Variation</Typography>
-          
-          <TextField xs={12} sm={6} md={4} lg={3} xl={2}
-                select
-                fullWidth
-                margin="normal"
-                value={productId}
-                onChange={(e) => setProductId(e.target.value)}
-                SelectProps={{
-                  native: true,
-                }}
-              >
-                <option value="">Select Product</option>
-                {products?.map((product) => (
-                  <option key={product._id} value={product._id}>
-                    {product.name}
-                  </option>
-                ))}
-              </TextField>
-          <TableContainer component={Paper} sx={{ mt: 3 }}>
-            <Table>
-              <TableHead>
-                <TableRow>
-                  <TableCell>variation Name</TableCell>
-                  <TableCell>Product Name</TableCell>
-                  <TableCell>Quantity</TableCell>
-                  <TableCell>Created At</TableCell>
-                  <TableCell>Item Include Qty</TableCell>
-                  <TableCell>Base Price</TableCell>
-                  <TableCell>One Day Premium</TableCell>
-                  <TableCell>Two Day Premium</TableCell>
-                  <TableCell>Base Price (XL)</TableCell>
+  // State to control image modal
+  const [openImageModal, setOpenImageModal] = useState(false);
+  const [selectedImages, setSelectedImages] = useState([]);
+
+  const handleOpenImageModal = (mainImage, additionalImages) => {
+    setSelectedImages([mainImage, ...additionalImages]);
+    setOpenImageModal(true);
+  };
+  // console.log(selectedImages)
+  const handleCloseImageModal = () => {
+    setOpenImageModal(false);
+    setSelectedImages([]);
+  };
+  const handleOpenEditProduct = (variation) => {
+    setSelectedVariation(variation);
+    setIsEditProductOpen(true);
+  };
+
+  const handleCloseEditProduct = () => {
+    setIsEditProductOpen(false);
+    setSelectedVariation(null);
+  };
+  const handleImageUpload = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setImagePreview(reader.result); // Set the preview URL
+        setSelectedVariation({ ...selectedVariation, mainImage: file.name }); // Store the file in selectedVariation
+      };
+      reader.readAsDataURL(file); // Read the file as a data URL
+    }
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+  
+    const formData = new FormData();
+    
+    // Append text fields
+    formData.append("name", selectedVariation?.name || "");
+    formData.append("quantity", selectedVariation?.quantity || "");
+    formData.append("price", JSON.stringify(selectedVariation.price));
+    formData.append("itemQty", selectedVariation?.itemQty || "");
+    if (selectedImages) {
+      formData.append("mainImage", selectedImages);
+    }
+  
+    // Append image if available
+
+  
+  
+  };
+  if (isLoading) return <Typography>Loading...</Typography>;
+
+  return (
+    <Box>
+      <Header title="Variation" subtitle="See your list of products." />
+      <Button variant="contained" onClick={handleOpenAddProduct}>
+        Add Variation
+      </Button>
+      <VariationFrom
+        open={isAddProductOpen}
+        handleClose={handleCloseAddProduct}
+      />
+      <Box sx={{ p: 2 }}>
+        <Typography variant="h5">Variation</Typography>
+
+        <TextField
+          xs={12}
+          sm={6}
+          md={4}
+          lg={3}
+          xl={2}
+          select
+          fullWidth
+          margin="normal"
+          value={productId}
+          onChange={(e) => setProductId(e.target.value)}
+          SelectProps={{
+            native: true,
+          }}
+        >
+          <option value="">Select Product</option>
+          {products?.map((product) => (
+            <option key={product._id} value={product._id}>
+              {product.name}
+            </option>
+          ))}
+        </TextField>
+        <TableContainer component={Paper} sx={{ mt: 3 }}>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>variation Name</TableCell>
+                <TableCell>Product Name</TableCell>
+                <TableCell>Quantity</TableCell>
+                <TableCell>Created At</TableCell>
+                <TableCell>Item Include Qty</TableCell>
+                <TableCell>Base Price</TableCell>
+                <TableCell>One Day Premium</TableCell>
+                <TableCell>Two Day Premium</TableCell>
+                {/* <TableCell>Base Price (XL)</TableCell>
                   <TableCell>One Day Premium (XL)</TableCell>
                   <TableCell>Two Day Premium (XL)</TableCell>
                   <TableCell>Base Price (MD)</TableCell>
                   <TableCell>One Day Premium (MD)</TableCell>
-                  <TableCell>Two Day Premium (MD)</TableCell>
-                  <TableCell>Actions</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {filteredVariations.map((item) => (
-                  <TableRow key={item._id}>
-                    <TableCell>{item.name}</TableCell>
-                    <TableCell>{item.productId.name}</TableCell>
-                    <TableCell>{item.quantity || "N/A"}</TableCell>
-                    <TableCell>{new Date(item.createdAt).toLocaleDateString()}</TableCell>
-                    <TableCell>{item.itemQty || "N/A"}</TableCell>
-                    {/* Base Price for Standard */}
-                    <TableCell>{item.price?.basePrice || "N/A"}</TableCell>
-                    <TableCell>{item.price?.oneDayPremium || "N/A"}</TableCell>
-                    <TableCell>{item.price?.twoDayPremium || "N/A"}</TableCell>
-                    {/* XL Price Details */}
-                    <TableCell>{item.xlPrice?.xlBasePrice || "N/A"}</TableCell>
+                  <TableCell>Two Day Premium (MD)</TableCell> */}
+                <TableCell>View Image</TableCell>
+                <TableCell>Edit</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {filteredVariations.map((item) => (
+                <TableRow key={item._id}>
+                  <TableCell>{item.name}</TableCell>
+                  <TableCell>{item.productId.name}</TableCell>
+                  <TableCell>{item.quantity || "N/A"}</TableCell>
+                  <TableCell>
+                    {new Date(item.createdAt).toLocaleDateString()}
+                  </TableCell>
+                  <TableCell>{item.itemQty || "N/A"}</TableCell>
+                  {/* Base Price for Standard */}
+                  <TableCell>{item.price?.basePrice || "N/A"}</TableCell>
+                  <TableCell>{item.price?.oneDayPremium || "N/A"}</TableCell>
+                  <TableCell>{item.price?.twoDayPremium || "N/A"}</TableCell>
+                  {/* XL Price Details */}
+                  {/* <TableCell>{item.xlPrice?.xlBasePrice || "N/A"}</TableCell>
                     <TableCell>{item.xlPrice?.xlOneDayPremium || "N/A"}</TableCell>
-                    <TableCell>{item.xlPrice?.xlTwoDayPremium || "N/A"}</TableCell>
-                    {/* MD Price Details */}
-                    <TableCell>{item.mdPrice?.mdBasePrice || "N/A"}</TableCell>
+                    <TableCell>{item.xlPrice?.xlTwoDayPremium || "N/A"}</TableCell> */}
+                  {/* MD Price Details */}
+                  {/* <TableCell>{item.mdPrice?.mdBasePrice || "N/A"}</TableCell>
                     <TableCell>{item.mdPrice?.mdOneDayPremium || "N/A"}</TableCell>
-                    <TableCell>{item.mdPrice?.mdTwoDayPremium || "N/A"}</TableCell>
-                    
-                    {/* View Image Button */}
-                    <TableCell>
-                      <Button
-                        variant="outlined"
-                        onClick={() => handleOpenImageModal(item.mainImage, item.additionalImages)}
-                      >
-                        View Images
-                      </Button>
-                    </TableCell>
-                  </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </TableContainer>
-  
-          {/* Modal for displaying images */}
-          <Modal
-            open={openImageModal}
-            onClose={handleCloseImageModal}
-            aria-labelledby="image-modal-title"
-            aria-describedby="image-modal-description"
-          >
-            <Box sx={{
-              p: 4,
-              backgroundColor: 'white',
-              margin: 'auto',
-              maxWidth: 500,
-              overflow: 'auto',  // in case images are large
-              borderRadius: 2
-            }}>
-              <Typography id="image-modal-title" variant="h6" component="h2">
-                Product Images
-              </Typography>
-              {selectedImages.map((image, index) => (
-                <img
-                src={`${baseUrl}/${image.replace(/\\/g, "/")}`} 
-                alt={`Product image ${index + 1}`}
-                style={{ width: '30%' }}
-              />
-              ))}
-              <Button onClick={handleCloseImageModal}>Close</Button>
-            </Box>
-          </Modal>
-        </Box>
-      </Box>
-    );
-  };
+                    <TableCell>{item.mdPrice?.mdTwoDayPremium || "N/A"}</TableCell> */}
 
-export default VariationIndex
+                  {/* View Image Button */}
+                  <TableCell>
+                    <Button
+                      variant="outlined"
+                      onClick={() =>
+                        handleOpenImageModal(
+                          item.mainImage,
+                          item.additionalImages
+                        )
+                      }
+                    >
+                      View Images
+                    </Button>
+                  </TableCell>
+                  <TableCell>
+                    <Button
+                      variant="outlined"
+                      onClick={() => handleOpenEditProduct(item)}
+                    >
+                      Edit
+                    </Button>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+
+        <Modal
+          open={isEditProductOpen}
+          onClose={handleCloseEditProduct}
+          aria-labelledby="edit-modal-title"
+          aria-describedby="edit-modal-description"
+        >
+          <Box
+            sx={{
+              p: 4,
+              backgroundColor: "gray",
+              margin: "auto",
+              maxWidth: 500,
+              borderRadius: 2,
+            }}
+          >
+            <Typography id="edit-modal-title" variant="h6" component="h2">
+              Edit Variation
+            </Typography>
+            <form onSubmit={handleSubmit}>
+              <TextField
+                label="Variation Name"
+                fullWidth
+                margin="normal"
+                value={selectedVariation?.name || ""}
+                onChange={(e) =>
+                  setSelectedVariation({
+                    ...selectedVariation,
+                    name: e.target.value,
+                  })
+                }
+              />
+              <TextField
+                label="Quantity"
+                fullWidth
+                margin="normal"
+                type="number"
+                value={selectedVariation?.quantity || ""}
+                onChange={(e) =>
+                  setSelectedVariation({
+                    ...selectedVariation,
+                    quantity: e.target.value,
+                  })
+                }
+              />
+              <TextField
+                label="Base Price"
+                fullWidth
+                margin="normal"
+                type="number"
+                value={selectedVariation?.price?.basePrice || ""}
+                onChange={(e) =>
+                  setSelectedVariation({
+                    ...selectedVariation,
+                    price: {
+                      ...selectedVariation.price,
+                      basePrice: e.target.value,
+                    },
+                  })
+                }
+              />
+              <TextField
+                label="One Day Premium"
+                fullWidth
+                margin="normal"
+                type="number"
+                value={selectedVariation?.price?.oneDayPremium || ""}
+                onChange={(e) =>
+                  setSelectedVariation({
+                    ...selectedVariation,
+                    price: {
+                      ...selectedVariation.price,
+                      oneDayPremium: e.target.value,
+                    },
+                  })
+                }
+              />
+              <TextField
+                label="Two Day Premium"
+                fullWidth
+                margin="normal"
+                type="number"
+                value={selectedVariation?.price?.twoDayPremium || ""}
+                onChange={(e) =>
+                  setSelectedVariation({
+                    ...selectedVariation,
+                    price: {
+                      ...selectedVariation.price,
+                      twoDayPremium: e.target.value,
+                    },
+                  })
+                }
+              />
+              <TextField
+                label="Item Include Qty"
+                fullWidth
+                margin="normal"
+                type="number"
+                value={selectedVariation?.itemQty || ""}
+                onChange={(e) =>
+                  setSelectedVariation({
+                    ...selectedVariation,
+                    itemQty: e.target.value,
+                  })
+                }
+              />
+              <input
+              type="file"
+              accept="image/*"
+              onChange={handleImageUpload}
+              style={{ margin: '10px 0' }}
+            />
+            {/* Display Main Image Preview */}
+            {imagePreview && (
+              <img
+                src={imagePreview}
+                alt="Main Variation Preview"
+                style={{ width: '80px', height: '80px', objectFit: 'cover', marginTop: '10px' }}
+              />
+            )}
+              {/* Add more fields as necessary */}
+              <Button type="submit" variant="contained" color="primary">
+                Save Changes
+              </Button>
+              <Button onClick={handleCloseEditProduct}>Cancel</Button>
+            </form>
+          </Box>
+        </Modal>
+
+        {/* Modal for displaying images */}
+        <Modal
+          open={openImageModal}
+          onClose={handleCloseImageModal}
+          aria-labelledby="image-modal-title"
+          aria-describedby="image-modal-description"
+        >
+          <Box
+            sx={{
+              p: 4,
+              backgroundColor: "white",
+              margin: "auto",
+              maxWidth: 500,
+              overflow: "auto", // in case images are large
+              borderRadius: 2,
+            }}
+          >
+            <Typography id="image-modal-title" variant="h6" component="h2">
+              Product Images
+            </Typography>
+            {selectedImages.map((image, index) => (
+              <img
+                src={`${baseUrl}/${image.replace(/\\/g, "/")}`}
+                alt={`Product image ${index + 1}`}
+                style={{ width: "30%" }}
+              />
+            ))}
+            <Button onClick={handleCloseImageModal}>Close</Button>
+          </Box>
+        </Modal>
+      </Box>
+    </Box>
+  );
+}
+
+export default VariationIndex;
